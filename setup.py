0ae3360924cd3bc8406845c9aa8b1686aa5d9d88
Martin Kersner <m.kersner@gmail.com>
2018-06-06 16:59:33 +0900

    add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(name="seoulai-gym",
       version="0.1",
       description="The Seoul AI Gym: Seoul AI Gym is a toolkit for developing AI algorithms.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/seoulai/gym",
       author="Seoul AI",
       author_email="m.kersner@gmail.com",
