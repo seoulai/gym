15f5954e06e30a3ecf181cd210942d1c042239d6
ayo79 <40293218+ayo79@users.noreply.github.com>
2018-08-03 00:29:18 +0900

    Update rules.py

--- seoulai_gym/envs/mighty/rules.py
+++ seoulai_gym/envs/mighty/rules.py
@@ -10,6 +10,8 @@ from typing import List
 from seoulai_gym.envs.mighty.base import Constants
 from seoulai_gym.envs.mighty.board import Board
 from copy import deepcopy
+
+
 class Rules(object):
     @staticmethod
     def get_valid_cards(
@@ -21,12 +23,12 @@ class Rules(object):
         valid_cards = deepcopy(board.PLAYER_CARDS[uid])
 
         # - 조커 사용, 효력, 조커콜
-        if uid != game.start_player and game.jokercall == True:
+        if uid != game.start_player and game.jokercall:
             for card in valid_cards:
                 if card == Constants.card_joker:
                     return [Constants.card_joker]
 
-        #- 해당 턴의 첫번째 플레이어가 낸 무늬만 낼 수 있음
+        # 해당 턴의 첫번째 플레이어가 낸 무늬만 낼 수 있음
         if game.start_player == uid:
             # - 주공 첫판 기루다x
             if game.president_player._uid == uid and game.round == 1:
@@ -34,19 +36,17 @@ class Rules(object):
                     if card[0] == game.giruda:
                         valid_cards.remove(card)
         else:
-            #start_player가 낸 무늬가 있으면 그것만 낼 수 있음
-            #turn_suit = board.FACE_CARDS[game.start_player][0]
-
+            # start_player가 낸 무늬가 있으면 그것만 낼 수 있음
             has_suit = False
             for card in valid_cards:
                 if card[0] == game.round_suit:
                     has_suit = True
                     break
 
-            if has_suit == True:
+            if has_suit:
                 for card in hand_cards:
                     if card[0] != game.round_suit:
-                        #마이티, 조커는 예외
+                        # 마이티, 조커는 예외
                         if card == game.mighty_card or card == Constants.card_joker:
                             pass
                         else:
@@ -55,7 +55,7 @@ class Rules(object):
                 # 없으면 다른 카드 낼 수 있음
                 pass
         if len(valid_cards) == 0:
-            Rules.get_valid_cards(uid,board,game)
+            Rules.get_valid_cards(uid, board, game)
         return valid_cards
 
     @staticmethod
@@ -77,14 +77,14 @@ class Rules(object):
         max_power = -1
 
         for player in board.FACE_CARDS:
-            power = Rules.get_power(board.FACE_CARDS[player],game,board)
+            power = Rules.get_power(board.FACE_CARDS[player], game, board)
             if power > max_power:
                 max_power = power
                 winner = player
         return winner
 
     @staticmethod
-    #카드 별 파워. 마이티 > 조커(일반) > 기루다 > 라운드 카드 > 조커(1라운드, 10라운드, 조커콜)
+    # 카드 별 파워. 마이티 > 조커(일반) > 기루다 > 라운드 카드 > 조커(1라운드, 10라운드, 조커콜)
     def get_power(card, game, board):
         power = 0
         if card == game.mighty_card:
@@ -92,11 +92,11 @@ class Rules(object):
         elif card == Constants.card_joker:
             if game.round == 1 or game.round == 10:
                 power = 0
-            elif game.jokercall == True:
+            elif game.jokercall:
                 power = 0
             else:
                 power = 999
-        elif card[0] == game.giruda: # game suit
+        elif card[0] == game.giruda:  # game suit
             card_num = card[2]
             if card_num == '1':
                 power = 14
@@ -125,7 +125,7 @@ class Rules(object):
             elif card_num == 'k':
                 power = 13
             power += 100
-        elif card[0] == game.round_suit: # trick_suit
+        elif card[0] == game.round_suit:  # trick_suit
             card_num = card[2]
             if card_num == '1':
                 power = 14
@@ -163,7 +163,10 @@ class Rules(object):
             face_cards: Dict,
     ) -> int:
         # TODO get_point
-        point_cards = ['s-1','s-k','s-q','s-j','s-0','d-1','d-k','d-q','d-j','d-0','c-1','c-k','c-q','c-j','c-0','h-1','h-k','h-q','h-j','h-0']
+        point_cards = ['s-1', 's-k', 's-q', 's-j', 's-0',
+                       'd-1', 'd-k', 'd-q', 'd-j', 'd-0',
+                       'c-1', 'c-k', 'c-q', 'c-j', 'c-0',
+                       'h-1', 'h-k', 'h-q', 'h-j', 'h-0']
         point_card = []
         for uid in face_cards:
             face_card = face_cards[uid]
