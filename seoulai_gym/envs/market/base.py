f849d2e3454770c6ce2895a48096d2f40bdfc241
James Park <laplacian.k@gmail.com>
2018-09-11 01:31:26 +0900

    added constants

--- seoulai_gym/envs/market/base.py
+++ seoulai_gym/envs/market/base.py
@@ -9,6 +9,7 @@ class Constants(object):
     """ Constants to share between classes and functions for market game.
         Actually It is not used now.
     """
-    BUY = "buy"
-    SELL = "sell"
-    HOLD = "hold"
+    BUY = 1 
+    SELL = 2 
+    HOLD = 0
+    DECISION = ['hold', 'buy', 'sell'] 
