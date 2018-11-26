c95f22ae1ee1478d2ffef815114c981b1d57df15
James Park <laplacian.k@gmail.com>
2018-11-27 00:12:05 +0900

    Update get request logic

--- seoulai_gym/envs/market/api.py
+++ seoulai_gym/envs/market/api.py
@@ -35,11 +35,14 @@ class BaseAPI():
         data,
     ):
         url = BASE_URL + cmd
+        conditions = [key+"="+value for key, value in data.items()]
+        query = "?" + "&".join(conditions)
+        url += query
         print(url, json.dumps(data))
-        headers = {"content-type": "application/json"}
-        r = requests.get(url,
-                         headers=headers,
-                         data=json.dumps(data))
+        # headers = {"content-type": "application/json"}
+        # print(url, json.dumps(data))
+        r = requests.get(url)
+
         if r.status_code == 200:
             return r.json()
         return None
